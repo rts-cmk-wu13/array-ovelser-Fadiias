2f0a9a74dc1a995848afa654bd86437dee07c1ab
Your Name <you@example.com>
2025-02-03 10:14:03 +0100

    update1

--- arrays.js
+++ arrays.js
@@ -5,10 +5,29 @@ document.addEventListener("DOMContentLoaded", function() {
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
     // --- Løs opgaverne herunder --- 
-const newnumbes=numbersArray.filter(function(number){
+const newnumbers=numbersArray.filter(function(number){
     return number>20 
 })
-console.log(newnumbes);
+console.log(newnumbers);
 
-    
+
+
+catsArray.push("Sniffles");
+console.log("Opgave 2", catsArray);
+
+
+const hasTrixie = dogsArray.includes("Trixie");
+console.log("Opgave 3a", hasTrixie);
+
+
+const hasBaxter = dogsArray.includes("Baxter");
+console.log("Opgave 3a", hasBaxter);
+
+
+const bagheera = catsArray.find(cat => cat === "Bagheera");
+console.log("Opgave 4a", bagheera);
+
+const salem = catsArray.find(cat => cat === "Salem");
+console.log("Opgave 4b", salem);
+
 }) // ends DOMContentLoaded
